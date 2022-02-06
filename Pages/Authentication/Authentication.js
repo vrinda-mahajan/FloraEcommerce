f74937a83e9dbd545e655572809acf047566b6a5
vrinda-mahajan <vrindamahajan96@gmail.com>
2022-02-07 00:51:59 +0530

    Added login and signup page

---
+++ Pages/Authentication/Authentication.js
@@ -0,0 +1,14 @@
+const login = document.querySelector('.link-login');
+const signUp = document.querySelector('.link-sign-up');
+const loginSection = document.querySelector('.login-section');
+const signUpSection = document.querySelector('.sign-up-section');
+signUp.addEventListener('click',() => {
+    // loginSection.classList.add('hide');
+    signUpSection.style.display = "block";
+    loginSection.style.display = "none";
+})
+
+login.addEventListener('click', () => {
+    signUpSection.style.display = "none"
+    loginSection.style.display = "block"
+} )
